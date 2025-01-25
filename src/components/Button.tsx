101249f27e17c2625a9ad26de9d1051b21e91efd
Sterphane Roque <stermiss2@gmail.com>
2025-01-25 03:33:24 -0300

    feat: button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,28 @@
+import { ButtonHTMLAttributes } from "react";
+import { tv } from "tailwind-variants";
+
+const button = tv({
+  base: "h-[40px] rounded-full px-8 font-semibold text-sm",
+  variants: {
+    variant: {
+      primary: "bg-pink-400 text-white ",
+      secondary: "bg-white text-gray-800",
+    },
+  },
+
+  defaultVariants: {
+    variant: "primary",
+  },
+});
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant: "primary" | "secondary";
+}
+
+export function Button({ variant, children, ...rest }: ButtonProps) {
+  return (
+    <button className={button({ variant })} {...rest}>
+      {children}
+    </button>
+  );
+}

9600eeee4e649d0fe7bcd57099d29d6d22ffdf44
Sterphane Roque <stermiss2@gmail.com>
2025-01-26 04:49:19 -0300

    feat: add type in input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,41 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, useState } from "react";
+import { LuEye, LuEyeOff } from "react-icons/lu";
 import { tv } from "tailwind-variants";
 
-export function Input({ ...rest }: InputHTMLAttributes<HTMLInputElement>) {
+export function Input({
+  type,
+  ...rest
+}: InputHTMLAttributes<HTMLInputElement>) {
+  const [isShowingPassword, setIsShowingPassword] = useState<boolean>(false);
+
+  function handleShowingPassword() {
+    setIsShowingPassword(false);
+  }
+  function handleNotShowingPassword() {
+    setIsShowingPassword(true);
+  }
+
   return (
-    <input
-      className="h-10 w-full max-w-64 rounded-lg border border-gray-300 px-5 focus-visible:outline-none placeholder:text-sm"
-      {...rest}
-    />
+    <div className="relative">
+      <input
+        type={type === "password" && isShowingPassword ? "text" : type}
+        className="h-10 w-full max-w-64 rounded-lg border border-gray-300 px-5 focus-visible:outline-none placeholder:text-sm"
+        {...rest}
+      />
+      {type === "password" &&
+        (isShowingPassword ? (
+          <LuEye
+            onClick={handleShowingPassword}
+            className="cursor-pointer absolute text-gray-500 right-2 top-0 h-full"
+            size={18}
+          />
+        ) : (
+          <LuEyeOff
+            onClick={handleNotShowingPassword}
+            className="cursor-pointer absolute text-gray-500 right-2 top-0 h-full"
+            size={18}
+          />
+        ))}
+    </div>
   );
 }

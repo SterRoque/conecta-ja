54f1265b230465e8bf10c390a3244016515e3f68
Sterphane Roque <stermiss2@gmail.com>
2025-01-26 03:52:45 -0300

    feat: new variants button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,26 +2,45 @@ import { ButtonHTMLAttributes } from "react";
 import { tv } from "tailwind-variants";
 
 const button = tv({
-  base: "h-[40px] w-fit rounded-full px-8 font-semibold text-sm",
+  base: "h-[40px]  px-8 font-semibold text-sm",
   variants: {
     variant: {
-      primary: "bg-pink-400 text-white ",
+      primary: "bg-pink-400 text-white",
       secondary: "bg-white text-gray-800",
+    },
+
+    rounded: {
+      sm: "rounded-lg",
+      full: "rounded-full",
+    },
+    size: {
+      small: "w-fit",
+      full: "w-full",
     },
   },
 
   defaultVariants: {
     variant: "primary",
+    rounded: "full",
+    size: "small",
   },
 });
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant: "primary" | "secondary";
+  variant?: "primary" | "secondary";
+  rounded?: "sm" | "full";
+  size?: "small" | "full";
 }
 
-export function Button({ variant, children, ...rest }: ButtonProps) {
+export function Button({
+  variant,
+  rounded,
+  size,
+  children,
+  ...rest
+}: ButtonProps) {
   return (
-    <button className={button({ variant })} {...rest}>
+    <button className={button({ variant, rounded, size })} {...rest}>
       {children}
     </button>
   );

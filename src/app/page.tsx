4c6ddd3edea08781086a46a4d430cbd96ab62cfc
Sterphane Roque <stermiss2@gmail.com>
2025-01-26 04:53:05 -0300

    feat: add modal in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import { Button } from "@/components/Button";
 import { Header } from "@/components/Header";
+import { Modal } from "@/components/Modal";
+import { useState } from "react";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
-      <Header />
+      <Header buttonOnClick={() => setIsOpen(true)} />
       <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col gap-5 justify-center">
           <h1 className="text-fuchsia-600 font-bold lg:text-5xl 2xl:text-6xl 2xl:w-[800px]">
@@ -21,6 +27,8 @@ export default function Home() {
           className="w-[550px] 2xl:w-[800px] h-fit mt-7"
         />
       </div>
+
+      <Modal onClose={() => setIsOpen(false)} isOpen={isOpen} />
     </div>
   );
 }
